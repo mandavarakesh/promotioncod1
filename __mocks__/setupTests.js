1ed78236259799648cdb3410f617c868d036bb97
mandavarakesh <111143421+mandavarakesh@users.noreply.github.com>
2025-04-22 13:45:48 +0530

    Delete __mocks__ directory

--- __mocks__/setupTests.js
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-
-global.matchMedia = global.matchMedia || function () {
-    return {
-        matches: false,
-        addListener: jest.fn(),
-        removeListener: jest.fn()
-    }
-}
